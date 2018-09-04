643a7fa6e332b5b3e8f86b8e340b6e9644a78a45
Aya <ayasaberosman@gmail.com>
2018-09-05 00:30:07 +0200

    add if condition examples

---
+++ session 3/prize.py
@@ -0,0 +1,21 @@
+"""
+Write an if statement that lets a competitor know which of these prizes they won based on the number of points they scored, which is stored in the integer variable points.
+
+Points	    Prize
+1 - 50	    wooden rabbit
+51 - 150    no prize
+151 - 180   wafer-thin mint
+181 - 200   penguin
+"""
+points = 174
+
+if points <= 50:
+    result = "Congratulations! You won a wooden rabbit!"
+elif points <= 150:
+    result = "Oh dear, no prize this time."
+elif points <= 180:
+    result = "Congratulations! You won a wafer-thin mint!"
+else:
+    result = "Congratulations! You won a penguin!"
+
+print(result)
